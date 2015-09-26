99e8ed3058365c75b51ef85a8ae626ba06d04b7d
andela-bomotoso <bukola.omotoso@andela.com>
2015-09-26 09:51:02 +0100

    Add the Member Class to the checkpoint.andela.main package

---
+++ src/checkpoint/andela/main/Member.java
@@ -0,0 +1,101 @@
+package checkpoint.andela.main;
+
+import java.sql.Date;
+
+/**
+ * Created by GRACE on 9/26/2015.
+ */
+public class Member {
+
+    /**
+     * Member Class Properties
+     */
+    private String fullName;
+    private char gender;
+    private String dateOfBirth;
+    private String  emailAddress;
+    private String phoneNumber;
+    private Date dateOfRegistration;
+
+    /**
+     * Default Constructor
+     */
+    public Member(){
+    }
+
+    /**
+     *  Constructor to set up Values
+     */
+    public Member(String fullName,char gender,String dateOfBirth, String emailAddress, String phoneNumber){
+        this.fullName =  fullName;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber =  phoneNumber;
+        //this.dateOfRegistration = dateOfRegistration;
+    }
+
+    // Get Member's Fullname
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    // Set Member's Fullname
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    // Get Member's Gender
+
+    public char getGender() {
+        return gender;
+    }
+
+    // Set Member's Gender
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    // Get Member's Date Of Birth
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    // Set Member's Date Of Birth
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // Get Member's Email Address
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    // Set Member's Email Address
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    // Get Member's Phone Number
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Set Members's Phone Number
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Get Member's Date Of Registration
+    public Date getDateOfRegistration() {
+        return dateOfRegistration;
+    }
+
+    // Set Member's Date Of Registration
+    public void setDateOfRegistration(Date dateOfRegistration) {
+        this.dateOfRegistration = dateOfRegistration;
+    }
+}
